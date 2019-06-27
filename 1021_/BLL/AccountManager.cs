0abf3c59cbac5d11affd0508d20d4840463af84c
Dipesh Mundra <35653005+D4dipesh@users.noreply.github.com>
2019-06-27 06:15:38 +0530

    Add files via upload

---
+++ 1021_/BLL/AccountManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using BOL;
+namespace BLL
+{
+    public class AccountManager
+    {
+        public static bool Validate(string userName, string password)
+        {
+            return AccountDal.Validate(userName, password);
+        }
+       
+        public static bool Register(string name, string speciality,double fee,DateTime birthDate ,string hospital, string username, string password)
+        {
+            return AccountDal.register(name, speciality,fee,birthDate, hospital, username, password);
+        }
+        public static List<Doctor> listAllDoc()
+        {
+            return AccountDal.listDoc();
+        }
+        public static bool delete(string userName)
+        {
+            return AccountDal.Delete(userName);
+        }
+        public static Doctor getByusername(string userName)
+        {
+            return AccountDal.getByUserName(userName);
+        }
+    }
+}
---
+++ 1021_/BOL/doctor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+    public class Doctor
+    {
+        #region member variables
+
+        protected string name;
+        protected string speciality;
+        protected DateTime birthDate;
+        protected double fee;
+        protected string hospital;
+        public string userName { get; set; }
+        public string password { get; set; }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Speciality
+        {
+            get { return speciality; }
+            set { speciality = value; }
+        }
+
+
+        public DateTime BirthDate
+        {
+            set { birthDate = value; }
+            get { return birthDate; }
+        }
+        public double Fee
+        {
+            get { return fee; }
+            set { fee = value; }
+        }
+        public string Hospital
+        {
+            get { return hospital; }
+            set { hospital = value; }
+        }
+        #endregion
+
+        #region constructors
+        public Doctor() { }
+        public Doctor(string Name, string sp,double fee,string hosp,string username,string password)
+        {
+            name = Name;
+            speciality = sp;
+           this.fee = fee;
+            hospital = hosp;
+            userName = username;
+            this.password = password;
+            
+        }
+
+        
+        #endregion
+
+        #region overridable functions
+        public override string ToString()
+        {
+            return name + " " + speciality+" "+fee+" "+hospital;
+        }
+
+        #endregion
+        ~Doctor()
+        {
+
+        }
+    }
+}
